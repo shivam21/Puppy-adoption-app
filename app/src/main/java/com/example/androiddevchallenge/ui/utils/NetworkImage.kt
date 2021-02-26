9439810f82af793136b25fca12dd22fe980e6487
Shivam Bhusri <shivam.bhusri@africa.airtel.com>
2021-02-26 16:09:02 +0530

    Remove wildcard imports

--- app/src/main/java/com/example/androiddevchallenge/ui/utils/NetworkImage.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/utils/NetworkImage.kt
@@ -9,7 +9,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
@@ -73,7 +77,6 @@ fun NetworkImage(
         }
     }
 }
-
 
 @Composable
 fun loadImage(
